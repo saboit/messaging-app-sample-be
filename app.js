bca4deaa73584c5721167de1b5da07b4e0f642cd
Boris Penko <boris.penko@saboit.de>
2020-06-25 13:32:52 +0200

    added basic methods

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser'); // parser for post requests
+const AssistantV2 = require('ibm-watson/assistant/v2'); // watson sdk
+const { IamAuthenticator } = require('ibm-watson/auth');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const cridentials = {
+  API_KEY: '9dLH10NOMpracXoD7RrGD_tqGfj0NxUHhPeDA6F72nTC',
+  URL:  'https://api.eu-gb.assistant.watson.cloud.ibm.com',
+  VERSION: '2019-02-28',
+  ASSISTANT_ID: '0dcecb0a-1aa3-444f-8201-caccdf94f3c6'
+}
+
+
+const authenticator = new IamAuthenticator({
+  apikey: cridentials.API_KEY
+})
+
+var assistant = new AssistantV2({
+  authenticator: authenticator,
+  url: cridentials.URL,
+  version: cridentials.VERSION,
+  disableSslVerification: true
+});
+
+
+
+app.post('/message', function (req, res) {
+  if (!req.body.sessionId || !req.body.message) {
+    res.status(400).send("bad request")
+  }
+  var payload = {
+    assistantId: cridentials.ASSISTANT_ID,
+    sessionId: req.body.sessionId,
+    input: {
+      message_type : 'text',
+      text : req.body.message
+    }
+  };      
+  assistant.message(payload, function(err, data) {
+    if (err) {
+      return res.status(err.code || 500).json(err)
+    }
+    return res.json(data)
+  })
+})
+
+
+
+
+app.get('/session_id', function (req, res) {
+  assistant.createSession(
+    {
+      assistantId: cridentials.ASSISTANT_ID,
+    },
+      function(error, response) {
+        if (error) {
+          return res.send(error);
+        } else {
+          return res.send(response);
+        }
+      }
+    );
+  })
+  
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
